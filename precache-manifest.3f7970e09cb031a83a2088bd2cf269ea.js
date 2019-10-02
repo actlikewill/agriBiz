f565eaa82975692da697e1efcb3b0762daeb9c6e
actlikewill <wilsongaturu@gmail.com>
2019-10-02 09:25:38 +0300

    Updates

---
+++ precache-manifest.3f7970e09cb031a83a2088bd2cf269ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212c8b5efc45d7866160f9e73af722c9",
+    "url": "/agriBiz/index.html"
+  },
+  {
+    "revision": "fd74c77bcd049e24c39c",
+    "url": "/agriBiz/static/css/main.ed28500f.chunk.css"
+  },
+  {
+    "revision": "1e11bc8a2fb1f596f691",
+    "url": "/agriBiz/static/js/2.aff6a7ae.chunk.js"
+  },
+  {
+    "revision": "fd74c77bcd049e24c39c",
+    "url": "/agriBiz/static/js/main.8a78f98b.chunk.js"
+  },
+  {
+    "revision": "06b4ed42ea745c27be86",
+    "url": "/agriBiz/static/js/runtime~main.ba053b16.js"
+  },
+  {
+    "revision": "41764122ea21ba6004d80cfd79328372",
+    "url": "/agriBiz/static/media/agribiz.logo.41764122.svg"
+  },
+  {
+    "revision": "aee0d9d4f5a6df948b5eac9853b4ecce",
+    "url": "/agriBiz/static/media/bg.aee0d9d4.svg"
+  }
+]);
